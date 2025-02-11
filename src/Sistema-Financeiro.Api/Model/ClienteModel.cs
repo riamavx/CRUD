07ed0d6a510422e9d94b8f5d0b682a633a4c06a0
riamvx <messenger.mariah2017@gmail.com>
2025-02-10 21:14:05 -0300

    :Construction: criação estrutura basica interfaces da service

--- src/Sistema-Financeiro.Api/Model/ClienteModel.cs
+++ src/Sistema-Financeiro.Api/Model/ClienteModel.cs
@@ -8,8 +8,8 @@ namespace Sistema_Financeiro.Api.Model
         public string Name { get; set; }
         public string CPF {  get; set; }
         public DateTime DataNascimento { get; set; }
-        public string Endereco { get; set; }
-        public string Telefone { get; set; }
+        public string? Endereco { get; set; }
+        public string? Telefone { get; set; }
         public string Email { get; set; }
         public DateTime DataCadastro { get; set; }
 
---
+++ src/Sistema-Financeiro.Api/Service/Interfaces/IClienteInterface.cs
@@ -0,0 +1,6 @@
+﻿namespace Sistema_Financeiro.Api.Service.Interfaces
+{
+    public interface IClienteInterface
+    {
+    }
+}
